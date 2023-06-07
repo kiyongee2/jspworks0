503d7ca2ae26379c0503c004f64d317f8434a1b5
kiyongee2 <kiyongee2@gmail.com>
2023-06-07 15:11:37 +0900

    address book end

--- AddressBook/src/main/java/address/AddrBookDAO.java
+++ AddressBook/src/main/java/address/AddrBookDAO.java
@@ -15,4 +15,34 @@ public class AddrBookDAO {
 	public ArrayList<AddrBook> getList(){
 		return addrList;
 	}
+	
+	//주소 상세보기
+	public AddrBook getAddrBook(String username) {
+		AddrBook addrBook = null;
+		
+		for(int i=0; i<addrList.size(); i++) { //전체를 순회하면서
+			//이미 저장된 이름을 변수로 할당
+			String dbUsername = addrList.get(i).getUsername();
+			if(dbUsername.equals(username)) { //외부 입력된 이름과 일치하면
+				addrBook = addrList.get(i);  //주소 1개를 가져옴
+				break;
+			}
+		}
+		return addrBook; //주소를 반환함
+	}
+	
+	//주소 삭제
+	public void deleteAddrBook(String username) {
+		AddrBook addrBook = null;
+		
+		for(int i=0; i<addrList.size(); i++) { //전체를 순회하면서
+			//이미 저장된 이름을 변수로 할당
+			String dbUsername = addrList.get(i).getUsername();
+			if(dbUsername.equals(username)) { //외부 입력된 이름과 일치하면
+				addrBook = addrList.get(i);  //주소 1개를 가져옴
+				addrList.remove(addrBook);   //주소 삭제
+				break;
+			}
+		}
+	}
 }
--- AddressBook/src/main/webapp/address/addrList.jsp
+++ AddressBook/src/main/webapp/address/addrList.jsp
@@ -17,7 +17,12 @@
 		<table id="tbl_list">
 			<thead>
 				<tr>
-					<th>이름</th><th>전화번호</th><th>이메일</th><th>성별</th>
+					<th>이름</th>
+					<th>전화번호</th>
+					<th>이메일</th>
+					<th>성별</th>
+					<th>보기</th>
+					<th>삭제</th>
 				</tr>
 			</thead>
 			<tbody>
@@ -31,6 +36,17 @@
 					<td><%=aB.getTel() %></td>
 					<td><%=aB.getEmail() %></td>
 					<td><%=aB.getGender() %></td>
+					<td>
+						<a href="addrView.jsp?username=<%=aB.getUsername() %>">
+							<button type="button">보기</button>
+						</a>
+					</td>
+					<td>
+					   <a href="addrDelete.jsp?username=<%=aB.getUsername() %>"
+					   	  onclick="return confirm('정말로 삭제하시겠습니까?')">
+					   	<button type="button">삭제</button>
+					   </a>
+					</td>
 				</tr>
 				<% } %>
 			</tbody>
