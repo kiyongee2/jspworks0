953c3c1fafeb6c8fbf713f59e673caf2bbf40144
kiyongee2 <kiyongee2@gmail.com>
2023-06-08 18:19:32 +0900

    mvc update

--- Jspwork/src/main/java/mvc/MVCTest.java
+++ Jspwork/src/main/java/mvc/MVCTest.java
@@ -1,6 +1,7 @@
 package mvc;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -16,9 +17,17 @@ public class MVCTest extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		String season = "여름";
+		String[] seasons = {"봄", "여름", "가을", "겨울"};
+		
+		ArrayList<Integer> numberList = new ArrayList<>();
+		numberList.add(10);
+		numberList.add(20);
+		numberList.add(30);
 		
 		//모델 생성해서 view에 보냄
 		request.setAttribute("season", season);
+		request.setAttribute("seasons", seasons);
+		request.setAttribute("numberList", numberList);
 		
 		//포워딩
 		RequestDispatcher dispatcher = 
